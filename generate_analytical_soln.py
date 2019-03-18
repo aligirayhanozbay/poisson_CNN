146aaa1af8203149ff4fcf064596abdcdda3e03f
Ali Girayhan Ozbay <ago14@cx1-15-4-6.cx1.hpc.ic.ac.uk>
2019-03-18 14:20:35 +0000

    checkpoint for multiprocessing fixes

--- generate_analytical_soln.py
+++ generate_analytical_soln.py
@@ -2,7 +2,11 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.eager as tfe
 import itertools, h5py
+import opt_einsum as oe
 from multiprocessing import Pool as ThreadPool
+opts = tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
+conf = tf.ConfigProto(gpu_options=opts)
+tf.enable_eager_execution(config=conf)
 
 class integrator_nd:
     '''
@@ -27,7 +31,7 @@ class integrator_nd:
         quadrature_x, quadrature_w = tuple([np.polynomial.legendre.leggauss(n_quadpts)[i].astype(np.float64) for i in range(2)]) #quadrature weights and points
         c = np.array([np.array(0.5*(domain[n+1] - domain[n]),dtype=np.float64) for n in range(0,len(domain),2)]) #scaling coefficients - for handling domains other than [-1,1] x [-1,1]
         d = np.array([np.array(0.5*(domain[n+1] + domain[n]),dtype=np.float64) for n in range(0,len(domain),2)])
-        self.quadpts = tf.constant(np.apply_along_axis(lambda x: x + d, 0, np.einsum('i...,i->i...',np.array(np.meshgrid(*list(itertools.repeat(quadrature_x,ndims)),indexing = 'xy')),c)).transpose(list(np.arange(1,ndims+1)) + [0]),dtype = tf.float64)
+        self.quadpts = tf.constant(np.apply_along_axis(lambda x: x + d, 0, oe.contract('i...,i->i...',np.array(np.meshgrid(*list(itertools.repeat(quadrature_x,ndims)),indexing = 'xy')),c, backend = 'tensorflow')).transpose(list(np.arange(1,ndims+1)) + [0]),dtype = tf.float64)
     
         self.quadweights = np.prod(c)*quadrature_w
     
@@ -49,12 +53,12 @@ def mode_coeff_calculation_multiprocessing_wrapper(args):
     
     coefficients = []
     for i in range(int(mplus1_pi_over_L.shape[0])):
-        integrand = lambda *vars: F(*vars) * tf.reduce_prod(tf.sin(np.einsum('i...,i->i...',np.stack(vars),mplus1_pi_over_L[i])),axis=0)
+        integrand = lambda *vars: F(*vars) * tf.reduce_prod(tf.sin(oe.contract('i...,i->i...',np.stack(vars),mplus1_pi_over_L[i],backend = 'tensorflow')),axis=0)
         coefficients.append(-integrator(integrand) * tf.cast(two_to_the_power_ndims,tf.float64) / (tf.cast(domain_volume,tf.float64) * tf.reduce_sum(tf.square(mplus1_pi_over_L[i]))))
     
     return tf.constant(np.array(coefficients), dtype = tf.float64)
 
-def generate_analytical_solution_homogeneous_bc(rhs = 'random', output_shape = (64,64), nmodes = (16,16), domain = [1,1], n_threads = 16, rhs_return = True, max_random_magnitude = np.inf):
+def generate_analytical_solution_homogeneous_bc(rhs = 'random', output_shape = (64,64), nmodes = (16,16), domain = [1,1], n_threads = 16, rhs_return = True, max_random_magnitude = np.inf, n_random = 1):
     '''
     Generates an analytical solution to the Poisson equation with homogeneous Dirichlet (i.e. 0) Boundary conditions in the box x_1 = [0, L_1], ..., x_n = [0, L_n]
     Solution strategy is Fourier series based as outlined in
@@ -79,13 +83,16 @@ def generate_analytical_solution_homogeneous_bc(rhs = 'random', output_shape = (
     #for i in args:
     #    print(i + ' = ' + str(values[i]))
     
-    coords = [np.linspace(0,domain[i], output_shape[i]) for i in range(len(output_shape))] #Evaluate coordinates along each axis
-    coord_meshes = np.array(np.meshgrid(*coords)) #Create meshgrid
+    coords = [tf.linspace(tf.constant(0.0,dtype = tf.float64),domain[i], output_shape[i]) for i in range(len(output_shape))] #Evaluate coordinates along each axis
+    coord_meshes = tf.stack(tf.meshgrid(*coords)) #Create meshgrid
     ndims = len(domain) #No of dims of function
-    mode_permutations = np.array(list(itertools.product(*[np.arange(nmodes[i]) for i in range(len(nmodes))]))) #Every permutation of Fourier modes along different axes possible
-    mplus1_pi_over_L = np.einsum('j,ij->ij',1/np.array(domain),(mode_permutations + 1)*np.pi) #compute (m_i+1)*pi/L_i
-    sine_vals = np.prod(np.sin(np.einsum('ij,j...->ij...', mplus1_pi_over_L, coord_meshes)),axis=1) #sin((m_1+1)*pi*x_1/L_1) * ... * sin((m_n+1)*pi*x_n/L_n))
+    mode_permutations = tf.constant(np.array(list(itertools.product(*[np.arange(nmodes[i]) for i in range(len(nmodes))]))), dtype = tf.float64) #Every permutation of Fourier modes along different axes possible
+    mplus1_pi_over_L = oe.contract('j,ij->ij',tf.constant(domain, dtype = tf.float64)**(-1),(mode_permutations + 1)*np.pi, backend = 'tensorflow') #compute (m_i+1)*pi/L_i
+    #print(mplus1_pi_over_L)
+    #print(coord_meshes)
+    sine_vals = tf.reduce_prod(tf.sin(oe.contract('ij,j...->ij...', mplus1_pi_over_L, coord_meshes, backend = 'tensorflow')),axis=1) #sin((m_1+1)*pi*x_1/L_1) * ... * sin((m_n+1)*pi*x_n/L_n))
     
+    print('checkpoint: sine vals generated')
     if rhs == 'random': #Generate random Fourier coefficients for a random RHS
         #For an n dimensional RHS function F(x_1,...x_n) = \sum_{m_1,...,m_n} (A_{m_1,...,m_n} * sin((m_1+1)*pi*x_1/L_1) * ... * sin((m_n+1)*pi*x_n/L_n))
         #\int_0^{L_n} ... \int_0^{L_1} F * sin((m_1+1)*pi*x_1/L_1) * ... * sin((m_n+1)*pi*x_n/L_n) dx_1 ... dx_n = A*L_1*...*L_n/2^n
@@ -94,20 +101,21 @@ def generate_analytical_solution_homogeneous_bc(rhs = 'random', output_shape = (
         rhs_function_coeffs = tf.multiply(2*tf.random.uniform(tf.stack([mode_permutations.shape[0]]),dtype = tf.float64)-1,tf.exp(-tf.reduce_sum(tf.cast(mode_permutations, tf.float64),axis=1))) #Random RHS function Fourier coefficients
         soln_function_coeffs = -tf.divide(rhs_function_coeffs, tf.reduce_sum(tf.square(mplus1_pi_over_L),axis=1)) #Random solution Fourier coefficients
         
-        rhs = np.einsum('i,i...->...', rhs_function_coeffs, sine_vals)
-        soln = np.einsum('i,i...->...', soln_function_coeffs, sine_vals)
+        rhs = oe.contract('i,i...->...', rhs_function_coeffs, sine_vals, backend = 'tensorflow')
+        soln = oe.contract('i,i...->...', soln_function_coeffs, sine_vals, backend = 'tensorflow')
         if max_random_magnitude != np.inf:
-            max_magnitude = min(max_random_magnitude,tf.reduce_max(tf.abs(rhs_function_coeffs)))
-            rhs *= max_magnitude
-            soln *= max_magnitude
+            scaling_factor = max_random_magnitude/tf.reduce_max(tf.abs(rhs))
+            rhs = rhs * scaling_factor
+            soln = soln * scaling_factor
             
+        print('checkpoint: random generator returning')
         if rhs_return:
             return rhs, soln
         else:
             return soln
         
     elif callable(rhs):
-        if np.prod(nmodes) % n_threads != 0:
+        if tf.reduce_prod(nmodes) % n_threads != 0:
             raise(ValueError('n_threads must divide reduce_prod(nmodes)'))
         
         pool = ThreadPool(n_threads) 
@@ -122,17 +130,15 @@ def generate_analytical_solution_homogeneous_bc(rhs = 'random', output_shape = (
 
         
         if rhs_return:
-            return rhs(*coord_meshes), np.einsum('i,i...->...', soln_function_coeffs, sine_vals)
+            return rhs(*coord_meshes), oe.contract('i,i...->...', soln_function_coeffs, sine_vals, backend = 'tensorflow')
         else:
-            return np.einsum('i,i...->...', soln_function_coeffs, sine_vals)
+            return oe.contract('i,i...->...', soln_function_coeffs, sine_vals, backend = 'tensorflow')
     else:
         raise(TypeError('rhs must either be a callable function to be evaluated or random'))
         
 def random_calculation_multiprocessing_wrapper(args):
     
-    opts = tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
-    conf = tf.ConfigProto(gpu_options=opts)
-    tf.enable_eager_execution(config=conf)
+
     
     batch_size = args[0]
     solution_generator_parameters = args[1]
@@ -176,14 +182,16 @@ if __name__ == '__main__':
     
     pool = ThreadPool(n_threads)
     t0 = time.time()
-    rhses, solns = zip(*pool.map(random_calculation_multiprocessing_wrapper, itertools.repeat([batch_size, params], n_threads)))
+    res = pool.map(random_calculation_multiprocessing_wrapper, zip(itertools.repeat(batch_size, n_threads), itertools.repeat(params, n_threads)))
+    #res = list(map(random_calculation_multiprocessing_wrapper, zip(itertools.repeat(batch_size, n_threads), itertools.repeat(params, n_threads))))
     #rhses, solns = zip(*list(map(random_calculation_multiprocessing_wrapper, itertools.repeat([batch_size, params], n_threads))))
+    #rhses, solns = generate_analytical_solution_homogeneous_bc(**params)
     t1 = time.time()
     print('Generation of training data took ' + str(t1-t0) + ' seconds')
-    pool.close()
+    #pool.close()
     
-    rhses = tf.expand_dims(np.concatenate(rhses), axis = 1)
-    solns = tf.expand_dims(np.concatenate(rhses), axis = 1)
+    rhses = tf.expand_dims(tf.stack(rhses), axis = 1)
+    solns = tf.expand_dims(tf.stack(solns), axis = 1)
     
     with h5py.File(outputpath, 'w') as hf:
         hf.create_dataset('soln', data=solns)
