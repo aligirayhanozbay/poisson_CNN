30ee47c8bec7d22a9a9ddc11b6e552a45a8c5cd4
Ali Girayhan Ozbay <aligirayhan.ozbay14@imperial.ac.uk>
2019-05-17 10:57:42 +0100

    integral loss abc

--- Dirichlet_BC_NN.py
+++ Dirichlet_BC_NN.py
@@ -7,8 +7,15 @@ import numpy as np
 from abc import ABC, abstractmethod
 
 class Model_With_Integral_Loss_ABC(ABC, tf.keras.models.Model):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    '''
+    Abstract class to implement integral loss.
+    '''
+    def __init__(self, mae_component_weight = 0.0, mse_component_weight = 0.0, n_quadpts= 20, Lp_norm_power = 2, **kwargs):
+        super().__init__( **kwargs)
+        self.mae_component_weight = mae_component_weight
+        self.mse_component_weight = mse_component_weight
+        self.n_quadpts = n_quadpts
+        self.p = Lp_norm_power
 
     def integral_loss(self, y_true, y_pred):
         try: #try except block to handle keras model init shenanigans
@@ -102,12 +109,8 @@ class SepConvBlock(tf.keras.models.Model):
 
 #best candidate
 class Dirichlet_BC_NN_2B(Model_With_Integral_Loss_ABC): #variant to include dx info
-    def __init__(self, data_format = 'channels_first', x_output_resolution = 256, n_sepconvblocks = 3, mae_component_weight = 0.0, mse_component_weight = 0.0, n_quadpts= 20, Lp_norm_power = 2):
-        super().__init__()
-        self.mae_component_weight = mae_component_weight
-        self.mse_component_weight = mse_component_weight
-        self.n_quadpts = n_quadpts
-        self.p = Lp_norm_power
+    def __init__(self, data_format = 'channels_first', x_output_resolution = 256, n_sepconvblocks = 3, **kwargs):
+        super().__init__(**kwargs)
         self.x_output_resolution = x_output_resolution
         self.data_format = data_format
         self.conv1d_0 = tf.keras.layers.Conv1D(filters=4, kernel_size=5, padding='same', activation=tf.nn.leaky_relu, data_format = data_format)
@@ -157,12 +160,8 @@ class Dirichlet_BC_NN_2B(Model_With_Integral_Loss_ABC): #variant to include dx i
         return self.conv2d_5(out)
     
 class Dirichlet_BC_NN_2C(Model_With_Integral_Loss_ABC): #variant to include dx AND Lx/Ly info
-    def __init__(self, data_format = 'channels_first', x_output_resolution = 256, n_sepconvblocks = 3, mae_component_weight = 0.0, mse_component_weight = 0.0, n_quadpts= 20, Lp_norm_power = 2):
-        super().__init__()
-        self.mae_component_weight = mae_component_weight
-        self.mse_component_weight = mse_component_weight
-        self.n_quadpts = n_quadpts
-        self.p = Lp_norm_power
+    def __init__(self, data_format = 'channels_first', x_output_resolution = 256, n_sepconvblocks = 3, **kwargs):
+        super().__init__(**kwargs)
         self.x_output_resolution = x_output_resolution
         self.data_format = data_format
         self.conv1d_0 = tf.keras.layers.Conv1D(filters=8, kernel_size=5, padding='same', activation=tf.nn.leaky_relu, data_format = data_format)
@@ -242,3 +241,107 @@ class Dirichlet_BC_NN_2C(Model_With_Integral_Loss_ABC): #variant to include dx A
             
             out = self.output_upsample_4([self.conv2d_4_2(self.conv2d_4_1(self.conv2d_4_0(out))), newshape])
             return self.conv2d_5_1(self.conv2d_5_0(out))
+
+class ResnetBlock(tf.keras.models.Model):
+    def __init__(self, data_format = 'channels_first', **conv_layer_args):
+        super().__init__()
+        self.conv0 = tf.keras.layers.Conv2D(padding = 'same', data_format = data_format, **conv_layer_args)
+        self.conv1 = tf.keras.layers.Conv2D(padding = 'same', data_format = data_format, **conv_layer_args)
+
+    def call(self, inp):
+        return inp + self.conv0(self.conv1(inp))
+        
+class ResampledConvolutionBlock(tf.keras.models.Model):
+    def __init__(self, resample_arg, data_format = 'channels_first', resize_method = tf.image.ResizeMethod.BICUBIC, use_resnetblocks = True, pre_upsample_conv_blocks = 2, post_upsample_conv_blocks = 2, **convblockargs):
+        super().__init__()
+        self.data_format = data_format
+        self.resample = Upsample2(resample_arg, data_format = data_format, resize_method = resize_method)
+        self.resize_to_original_shape = Upsample2([-1, -1], data_format = data_format)
+        if use_resnetblocks:
+            self.pre_upsample_conv_blocks = [ResnetBlock(data_format = data_format, **convblockargs) for i in range(pre_upsample_conv_blocks)]
+            self.post_upsample_conv_blocks = [ResnetBlock(data_format = data_format, **convblockargs) for i in range(post_upsample_conv_blocks)]
+        else:
+            self.pre_upsample_conv_blocks = [tf.keras.layers.Conv2D(data_format = data_format, **convblockargs) for i in range(pre_upsample_conv_blocks)]
+            self.post_upsample_conv_blocks = [tf.keras.layers.Conv2D(data_format = data_format, **convblockargs) for i in range(post_upsample_conv_blocks)]
+        
+    def call(self, inp):
+        if self.data_format == 'channels_first':
+            original_shape = [inp.shape[2], inp.shape[3]]
+        else:
+            original_shape = [inp.shape[1], inp.shape[2]]
+        out = self.resample(inp)
+        for convblock in self.pre_upsample_conv_blocks:
+            out = convblock(out)
+        out = self.resize_to_original_shape([out, original_shape])
+        for convblock in self.post_upsample_conv_blocks:
+            out = convblock(out)
+        return out
+                                                  
+class Dirichlet_BC_NN_2D(Model_With_Integral_Loss_ABC):
+    def __init__(self, data_format = 'channels_first', x_output_resolution = 256, **kwargs):
+        super().__init__(**kwargs)
+        self.x_output_resolution = x_output_resolution
+        self.data_format = data_format
+        
+        self.conv1d_0 = tf.keras.layers.Conv1D(filters=8, kernel_size=5, padding='same', activation=tf.nn.leaky_relu, data_format = data_format)
+        self.conv1d_1 = tf.keras.layers.Conv1D(filters=64, kernel_size=5, padding='same', activation=tf.nn.leaky_relu, data_format = data_format)
+        self.conv1d_2 = tf.keras.layers.Conv1D(filters=256, kernel_size=5, padding='same', activation=tf.nn.leaky_relu, data_format = data_format)
+        self.resnetblocks_0 = [ResnetBlock(data_format = data_format, filters = 16, kernel_size = 5, activation = tf.nn.leaky_relu) for i in range(3)]
+                                                  
+        self.dx_dense_0 = tf.keras.layers.Dense(100, activation = tf.nn.leaky_relu)
+        self.dx_dense_1 = tf.keras.layers.Dense(256, activation = tf.nn.leaky_relu)
+        self.dx_dense_2 = tf.keras.layers.Dense(32*128, activation = tf.nn.leaky_relu)
+        self.dx_resampled_conv_blocks = [ResampledConvolutionBlock([0.5**i, 0.5**i], data_format = data_format, filters = 16, kernel_size = 3, activation = tf.nn.leaky_relu) for i in range(4)]
+        self.dx_upsample = Upsample2([-1, -1], data_format = data_format)
+
+        self.resnetblocks_1 = [ResnetBlock(data_format = data_format, filters = 16, kernel_size = 5, activation = tf.nn.leaky_relu) for i in range(3)]
+        
+        self.conv2d_0 = tf.keras.layers.Conv2D(filters = 8, kernel_size = 5, activation = tf.nn.leaky_relu, padding = 'same', data_format = data_format)
+        self.conv2d_1 = tf.keras.layers.Conv2D(filters = 4, kernel_size = 5, activation = tf.nn.leaky_relu, padding = 'same', data_format = data_format)
+        self.conv2d_2 = tf.keras.layers.Conv2D(filters = 1, kernel_size = 5, activation = tf.nn.tanh, padding = 'same', data_format = data_format)
+                                            
+    def call(self, inputs):
+        self.dx = inputs[1]
+        out = self.conv1d_2(self.conv1d_1(self.conv1d_0(inputs[0])))
+        try:
+            self.x_output_resolution = inputs[2]
+        except:
+            pass
+        
+        try: #stupid hack 1 to get past keras '?' tensor dimensions via try except block
+            if self.data_format == 'channels_first':
+                self.domain_info = oe.contract('ij,j->ij',tf.tile(inputs[1], [1,3]), tf.stack([tf.constant(1.0, dtype = tf.keras.backend.floatx()), tf.cast(self.x_output_resolution, tf.keras.backend.floatx()), tf.cast(tf.constant(inputs[0].shape[2]), tf.keras.backend.floatx())], axis = 0), backend = 'tensorflow')
+                self.input_length = inputs[0].shape[-1]
+                out = tf.expand_dims(out, axis = 1)
+                dx_res = tf.reshape(self.dx_dense_2(self.dx_dense_1(self.dx_dense_0(self.domain_info))), (-1, 1, 128, 32))
+            else:
+                self.domain_info = oe.contract('ij,j->ij',tf.tile(inputs[1], [1,3]), tf.stack([tf.constant(1.0, dtype = tf.keras.backend.floatx()), tf.cast(self.x_output_resolution, tf.keras.backend.floatx()), tf.cast(tf.constant(inputs[0].shape[1]), tf.keras.backend.floatx())], axis = 0), backend = 'tensorflow')
+                self.input_length = inputs[0].shape[-2]
+                out = tf.expand_dims(out, axis = 3)
+                dx_res = tf.reshape(self.dx_dense_2(self.dx_dense_1(self.dx_dense_0(self.domain_info))), (-1, 128, 32, 1))
+        except:
+            if self.data_format == 'channels_first':
+                self.domain_info = tf.tile(inputs[1], [1,3])
+                self.input_length = inputs[0].shape[-1]
+                out = tf.expand_dims(out, axis = 1)
+                dx_res = tf.reshape(self.dx_dense_2(self.dx_dense_1(self.dx_dense_0(self.domain_info))), (-1, 1, 128, 32))
+            else:
+                self.domain_info = tf.tile(inputs[1], [1,3])
+                self.input_length = inputs[0].shape[-2]
+                out = tf.expand_dims(out, axis = 3)
+                dx_res = tf.reshape(self.dx_dense_2(self.dx_dense_1(self.dx_dense_0(self.domain_info))), (-1, 128, 32, 1))
+        
+        for rb in self.resnetblocks_0:
+            out = rb(out)
+        try:
+            out = self.dx_upsample([sum([rcb(dx_res) for rcb in self.dx_resampled_conv_blocks]), [self.x_output_resolution, self.input_length]]) + self.dx_upsample([out, [self.x_output_resolution, self.input_length]])
+        except:
+            out = self.dx_upsample([sum([rcb(dx_res) for rcb in self.dx_resampled_conv_blocks]), [64, self.input_length]]) + self.dx_upsample([out, [64, self.input_length]])
+        for rb in self.resnetblocks_1:
+            out = rb(out)
+
+        return self.conv2d_2(self.conv2d_1(self.conv2d_0(out)))
+
+            
+        
+        
--- Homogeneous_Poisson_NN.py
+++ Homogeneous_Poisson_NN.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from Dirichlet_BC_NN import Model_With_Integral_Loss_ABC
 from MergeWithAttention import MergeWithAttention2
 from Upsample import Upsample2
 from Lp_integral_norm import Lp_integral_norm
@@ -82,13 +83,9 @@ class Homogeneous_Poisson_NN(tf.keras.models.Model):
     
     
     
-class Homogeneous_Poisson_NN_2(tf.keras.models.Model): #variant to include dx info
-    def __init__(self, pooling_block_number = 6, resize_methods = None, data_format = 'channels_first', n_quadpts = 20, Lp_norm_power = 2, mae_component_weight = 0.0, mse_component_weight = 0.0):
-        super().__init__()
-        self.mae_component_weight = mae_component_weight
-        self.mse_component_weight = mse_component_weight
-        self.n_quadpts = n_quadpts
-        self.p = Lp_norm_power
+class Homogeneous_Poisson_NN_2(Model_With_Integral_Loss_ABC): #variant to include dx info
+    def __init__(self, pooling_block_number = 6, resize_methods = None, data_format = 'channels_first', **kwargs):
+        super().__init__(**kwargs)
         self.pooling_block_number = pooling_block_number
         self.data_format = data_format
         self.pooling_block_kernel_sizes = 3 * np.ones((self.pooling_block_number), dtype = np.int32)
@@ -125,104 +122,3 @@ class Homogeneous_Poisson_NN_2(tf.keras.models.Model): #variant to include dx in
         out = self.conv_2(self.conv_1(inp))
         out = self.merge([self.conv_3(out)] + [pb(out) for pb in self.pooling_blocks])
         return self.conv_6(self.conv_5(tf.einsum('ijkl, ij -> ijkl',self.conv_4(out), 1.0*(0.0+self.dx_dense_2(self.dx_dense_1(self.dx_dense_0(self.dx)))))))
-
-    #@tf.function
-    def integral_loss(self, y_true, y_pred):
-        try:
-            if self.data_format == 'channels_first':
-                c = 0.5* tf.concat([self.dx * int(y_true.shape[-2]), self.dx * int(y_true.shape[-1])],1)
-                coords = np.array(np.meshgrid(np.linspace(-1, 1, y_true.shape[-2]),np.linspace(-1, 1, y_true.shape[-1]),indexing = 'xy'), dtype = np.float64).transpose((1,2,0)) #coordinates of each grid pt in the domain
-            else:
-                c = 0.5 * tf.concat([self.dx * int(y_true.shape[-3]), self.dx * int(y_true.shape[-2])],1)
-                coords = np.array(np.meshgrid(np.linspace(-1, 1, y_true.shape[-3]),np.linspace(-1, 1, y_true.shape[-2]),indexing = 'xy'), dtype = np.float64).transpose((1,2,0)) #coordinates of each grid pt in the domain
-        except:
-            return 0.0*(y_true - y_pred)
-        image_coords = [coords[0,:,0], coords[:,1,1]] #x and y coordinates separately
-        quadrature_x, quadrature_w = tuple([np.polynomial.legendre.leggauss(self.n_quadpts)[i].astype(np.float64) for i in range(2)])
-
-        quadpts = tf.constant(np.array(np.meshgrid(quadrature_x,quadrature_x,indexing = 'xy')).transpose((1,2,0)),dtype = tf.float64)
-        #quadweights = tf.reduce_prod(c)*tf.tensordot(tf.squeeze(quadrature_w),tf.squeeze(quadrature_w),axes = 0)
-        indices = [[],[]] #indices between each quadrature point lies - indices[0] is in x-dir and indices[1] is in the y-dir
-        quad_coords = [quadpts[0,:,0], quadpts[:,1,1]] #x and y coordinates of each quad pt respectively
-        #find the indices of coords between which every quad. pt. lies
-        for i in range(len(indices)):
-            j=0
-            #does not work if more than 2 quad pts fall within 1 cell - fix later
-            while len(indices[i]) < quadpts.shape[0] and j<image_coords[i].shape[0]:
-                try:
-                    if abs(float(quad_coords[i][len(indices[i])] - image_coords[i][j])) == float(min(abs(quad_coords[i][len(indices[i])] - image_coords[i][j-1]), abs(quad_coords[i][len(indices[i])] - image_coords[i][j]), abs(quad_coords[i][len(indices[i])] - image_coords[i][j+1]))):
-                        if quad_coords[i][len(indices[i])] - image_coords[i][j] < 0:
-                            indices[i].append((j-1,j))
-                        else:
-                            indices[i].append((j,j+1))
-                except:
-                    if abs(float(quad_coords[i][len(indices[i])] - image_coords[i][j])) == float(min(abs(quad_coords[i][len(indices[i])] - image_coords[i][j-1]), abs(quad_coords[i][len(indices[i])] - image_coords[i][j]))):
-                        indices[i].append((j-1,j))
-                j+=1
-        
-        index_combinations = tf.Variable(tf.zeros((quadpts.shape[0], quadpts.shape[1], 4 , 2), dtype = tf.int32), trainable = False, dtype = tf.int32) #array storing the 4 index combinations on the original grid which surround each quad. pt.
-        corners = tf.Variable(tf.zeros((quadpts.shape[0], quadpts.shape[1], 2 , 2), dtype = np.int32), dtype = tf.int32, trainable = False) #array storing the lower left corner and the upper right corner of each box stored in index_combinations. effectively this will contain [[xmin,ymin],[xmax,ymax]] for the rectangle around each quad pt.
-        s=tf.constant(indices)
-        for i in range(self.n_quadpts):
-            for j in range(self.n_quadpts):
-                index_combinations[i,j,:,:].assign(np.array(list(itertools.product(np.array(s)[0,i,:],np.array(s)[1,j,:]))))
-        for i in range(self.n_quadpts):
-            for j in range(self.n_quadpts):
-                corners[i,j,:,:].assign(tf.stack([s[0,i,:],s[1,j,:]]))
-        corners = tf.transpose(corners,(0,1,3,2))
-        corner_coords = tf.gather_nd(tf.transpose(coords,(1,0,2)),corners)
-        
-        #compute the coefficients [b_11,b_12,b_21,b_22]
-        #steps:
-        #1. compute transpose(invert(array([[1,xmin,ymin,xmin*ymin],[1,xmin,ymax,xmin*ymax],[1,xmax,ymin,xmax*ymin],[1,xmax,ymax,xmax*ymax]]))) for the rectangle around each quad pt.
-        #2. compute array([1,x_quadpt, y_quadpt, x_quadpt*y_quadpt]) for each quadpt
-        #3. multiply the result of 1 and 2 for each quad pt.
-        interpolation_matrix = tf.Variable(tf.ones((self.n_quadpts,self.n_quadpts,4,4), dtype = tf.keras.backend.floatx()), dtype = tf.keras.backend.floatx())
-        interpolation_matrix[:,:,0:2,1].assign(oe.contract('ijk,ij->ijk',interpolation_matrix[:,:,0:2,1],corner_coords[:,:,0,0], backend = 'tensorflow'))
-        interpolation_matrix[:,:,2:,1].assign(oe.contract('ijk,ij->ijk',interpolation_matrix[:,:,2:,1],corner_coords[:,:,1,0], backend = 'tensorflow'))
-        interpolation_matrix[:,:,0::2,2].assign(oe.contract('ijk,ij->ijk',interpolation_matrix[:,:,0::2,2],corner_coords[:,:,0,1], backend = 'tensorflow'))
-        interpolation_matrix[:,:,1::2,2].assign(oe.contract('ijk,ij->ijk',interpolation_matrix[:,:,1::2,2], corner_coords[:,:,1,1], backend = 'tensorflow'))
-        interpolation_matrix[:,:,:,3].assign(oe.contract('...,...,...->...',interpolation_matrix[:,:,:,3],interpolation_matrix[:,:,:,1], interpolation_matrix[:,:,:,2], backend = 'tensorflow'))
-        interpolation_matrix = tf.transpose(tf.linalg.inv(interpolation_matrix), (0,1,3,2))
-        q = tf.Variable(tf.ones((self.n_quadpts,self.n_quadpts,4), dtype = tf.keras.backend.floatx()), dtype = tf.keras.backend.floatx())
-        q[:,:,1].assign(tf.transpose(quadpts[:,:,0]))
-        q[:,:,2].assign(tf.transpose(quadpts[:,:,1]))
-        q[:,:,3].assign(tf.multiply(q[:,:,1],q[:,:,2]))
-        
-        b = oe.contract('ijkl, ijl->ijk', interpolation_matrix, q, backend = 'tensorflow')
-
-
-        quadweights = oe.contract('i,j,k->ijk',tf.squeeze(quadrature_w),tf.squeeze(quadrature_w), tf.reduce_prod(c,axis = 1), backend = 'tensorflow')
-        if self.data_format == 'channels_first':
-            interp_pts = tf.squeeze(tf.gather_nd(tf.transpose(y_true - y_pred, (2,3,1,0)), index_combinations), axis = 3)
-        else:
-            interp_pts = tf.squeeze(tf.gather_nd(tf.transpose(y_true - y_pred, (1,2,3,0)), index_combinations), axis = 3)
-
-        # print('---data---')
-        # print((y_true - y_pred).shape)
-        # print('---index combinations---')
-        # print(index_combinations.shape)
-        # print('---interp_pts---')
-        # print(interp_pts.shape)
-        # print('---quadweights---')
-        # print(quadweights[...,0])
-        # print('---b---')
-        # print(b.shape)
-
-        values_at_quad_pts = oe.contract('ijkl, ijk->ijl', interp_pts, b, backend = 'tensorflow')
-
-        loss = tf.reduce_mean(tf.reduce_sum(tf.multiply(quadweights, values_at_quad_pts**self.p), axis = (0,1))**(1/self.p))
-        # print('---values_at_quad_pts---')
-        # print(values_at_quad_pts.shape)
-        if self.mae_component_weight != 0.0:
-            loss = loss + self.mae_component_weight * tf.reduce_mean(tf.keras.losses.mean_absolute_error(y_true, y_pred))
-        if self.mse_component_weight != 0.0:
-            loss = loss + self.mse_component_weight * tf.reduce_mean(tf.keras.losses.mean_squared_error(y_true, y_pred))
-        return loss
-
-        
-    
-#    def integral_loss(self, y_true, y_pred):
-#        losses = [Lp_integral_norm(y_true.shape[-2:], [0, self.dx[i,0]*y_true.shape[-2],0, self.dx[i,0]*y_true.shape[-1]], n_quadpts = 20, mse_component_weight = 1e+1) for i in range(y_true.shape[0])]
-#        losses = [losses[i](tf.expand_dims(y_true[i], axis = 0), tf.expand_dims(y_pred[i], axis = 0)) for i in range(y_true.shape[0])]
-#        return tf.reduce_mean(losses)
