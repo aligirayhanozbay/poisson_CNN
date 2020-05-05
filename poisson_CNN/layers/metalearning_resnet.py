4d54e24f9a4ebb97f0d28cc8e8cc9616b60dd45e
Ali Girayhan Ozbay <aligirayhan.ozbay14@imperial.ac.uk>
2020-05-05 17:15:13 +0100

    metalearning layer version of resnet block

---
+++ poisson_CNN/layers/metalearning_resnet.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+from .metalearning_conv import metalearning_conv
+from .metalearning_deconvupscale import metalearning_deconvupscale
+
+class metalearning_resnet(tf.keras.models.Model):
+    def __init__(self, previous_layer_filters, filters, kernel_size, **other_metalearning_conv_args):
+        super().__init__()
+
+        self.conv0 = metalearning_conv(previous_layer_filters, filters, kernel_size, padding = 'same', **other_metalearning_conv_args)
+        self.conv1 = metalearning_conv(previous_layer_filters, filters, kernel_size, padding = 'same', **other_metalearning_conv_args)
+
+    @tf.function
+    def call(self, inp):
+        return inp[0] + self.conv1(self.conv0(inp))
