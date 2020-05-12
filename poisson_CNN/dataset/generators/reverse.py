a6ca41f2aab01c132b4c4acc06e9798ba1ed8ecc
Ali Girayhan Ozbay <aligirayhan.ozbay14@imperial.ac.uk>
2020-05-12 16:16:29 +0100

    data generation now working

--- poisson_CNN/dataset/generators/reverse.py
+++ poisson_CNN/dataset/generators/reverse.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
-import itertools
 import numpy as np
-import multiprocessing
-from collections.abc import Iterable
+import math
+import string
 
 from ..utils import *
 
@@ -62,8 +61,48 @@ def choose_conv_method(ndims):
     else:
         raise(NotImplementedError('Convolutions above 3D are not available yet'))
 
+@tf.function
+def generate_polynomial_and_second_derivative(roots,degree,npts,domain_size):
+    clip_value = 1e-15 if tf.keras.backend.floatx() == tf.float64 else 1e-6
+    coords = tf.linspace(tf.constant(0.0+clip_value,dtype=tf.keras.backend.floatx()),domain_size+clip_value,npts)
+    coords_expanded = tf.expand_dims(coords,1)
+    roots = tf.expand_dims(roots[:degree],0)
+    factors = coords_expanded + roots
+    p = tf.reduce_prod(factors,1)
+    dp = tf.gradients(p,coords)[0]
+    ddp = tf.gradients(dp,coords)[0]
+    return p,ddp
+
+@tf.function
+def batch_generate_polynomial_and_second_derivative(roots,degrees,npts,domain_size):
+    return tf.map_fn(lambda x: generate_polynomial_and_second_derivative(x[0],x[1],npts,domain_size), (roots, degrees), dtype=(tf.keras.backend.floatx(),tf.keras.backend.floatx()))
+
+@tf.function
+def polynomials_and_their_2nd_derivatives(npts, poly_deg, domain_sizes, batch_size = 1, homogeneous_bc = False):
+    '''
+    Creates a batch batch_size of polynomials with degree poly_deg sampled on npts equispaced points within domains [0,domain_sizes[k]] and also returns these polynomials' 2nd derivatives as computed by tensorflow autodifferentiation.
+
+    -npts: int. No of grid points in the output.
+    -poly_deg: int. Degree of the polynomial.
+    -domain_sizes: tf.Tensor of shape (batch_size,). determines the domains in which the polynomials are generated.
+    -batch_size: int. No of samples to generate.
+    -homogeneous_bc: bool. If enabled, the generated polynomials will have value 0 at the start and end of the domain.
+    '''
+    npts = npts * tf.ones((batch_size,),dtype=tf.int32)
+        
+    poly_deg -= 1
+    if homogeneous_bc:
+        roots = -tf.random.uniform((batch_size,poly_deg,poly_deg-1),dtype=tf.keras.backend.floatx())
+        roots = tf.concat([tf.zeros((batch_size,tf.shape(roots)[1],1),dtype=tf.keras.backend.floatx()),-tf.ones((batch_size,tf.shape(roots)[1],1),dtype=tf.keras.backend.floatx()),roots],-1)
+    else:
+        roots = -tf.random.uniform((batch_size,poly_deg,poly_deg+2),dtype=tf.keras.backend.floatx())
+    roots = tf.einsum('i,i...->i...',domain_sizes,roots)
+    degrees = tf.tile(tf.expand_dims(tf.range(2,poly_deg+2),0),[batch_size,1])
+    p,ddp = tf.map_fn(lambda x: batch_generate_polynomial_and_second_derivative(x[0],x[1],x[2],x[3]), (roots,degrees,npts,domain_sizes), dtype=(tf.keras.backend.floatx(),tf.keras.backend.floatx()))
+    return p,ddp
+
 class reverse_poisson_dataset_generator(tf.keras.utils.Sequence):
-    def __init__(self, batch_size, batches_per_epoch, output_size_range, coeff_grid_size_range, grid_spacings_range = None, normalize_domain_size = False, ndims = None, stencil_size = 5, homogeneous_bc = False, return_rhses = True, return_boundaries = True, return_dx = True, max_magnitude = 1.0):
+    def __init__(self, batch_size, batches_per_epoch, output_size_range, fourier_coeff_grid_size_range, taylor_degree_range, grid_spacings_range = None, normalize_domain_size = False, ndims = None, stencil_size = 5, homogeneous_bc = False, return_rhses = True, return_boundaries = True, return_dx = True, max_magnitude = 1.0):
         '''
         Generates batches of random Poisson equation RHS-BC-solutions by first generating a solution and then using finite difference schemes to generate the RHS. Smooth results are ensured by using a Fourier series approach.
 
@@ -71,7 +110,8 @@ class reverse_poisson_dataset_generator(tf.keras.utils.Sequence):
         -batch_size: int. Number of samples to generate each time __getitem__ is called
         -batches_per_epoch: int. Determines the number of batches to generate per keras epoch. New, random data are generated each time __getitem__ is called, so this is mostly for compatibility purposes
         -output_size_range: List of 2 ints, list of list of 2 ints, or np.ndarray/tf.Tensor of shape (ndims,2). Determines the range of random values from which the size of each spatial dimension will be picked for each batch (i.e. random value range for the shape of the  spatial dimensions)
-        -control_pt_grid_size_range: List of 2 ints, list of list of 2 ints, or np.ndarray/tf.Tensor of shape (ndims,2). Same as output_size range, but determines the range for the control pt grid size. Higher values create less smooth outputs.
+        -fourier_coeff_grid_size_range: List of 2 ints, list of list of 2 ints, or np.ndarray/tf.Tensor of shape (ndims,2). Determines the range from which the number of Fourier coefficients per dimension will be drawn
+        -taylor_coeff_grid_size_range: same as fourier_coeff_grid_size_range but for taylor series component
         -grid_spacings_range:  List of 2 floats, list of list of 2 floats, or np.ndarray/tf.Tensor of shape (ndims,2). Determines the range of values that the grid spacings can take for each dimension.
         -ndims: int. Number of spatial dimensions.
         -stencil_size: Odd int or list of odd ints. Determines the size of the FD stencil to be used for each dimension.
@@ -83,7 +123,7 @@ class reverse_poisson_dataset_generator(tf.keras.utils.Sequence):
         '''
         self.batch_size = batch_size
         if ndims is None:
-            for k in [output_size_range,coeff_grid_size_range]:
+            for k in [output_size_range,fourier_coeff_grid_size_range]:
                 try:
                     ndims = len(k)
                 except:
@@ -93,7 +133,10 @@ class reverse_poisson_dataset_generator(tf.keras.utils.Sequence):
 
         self.grid_spacings_range = handle_grid_parameters_range(grid_spacings_range, ndims)
         self.output_size_range = handle_grid_parameters_range(output_size_range, ndims)
-        self.coeff_grid_size_range = handle_grid_parameters_range(coeff_grid_size_range, ndims)
+        self.fourier_coeff_grid_size_range = handle_grid_parameters_range(fourier_coeff_grid_size_range, ndims)
+
+        self.taylor_degree_range = handle_grid_parameters_range(taylor_degree_range, ndims)
+        self.taylor_einsum_str = 'B' + ',B'.join(list(string.ascii_lowercase[-self.ndims:])) + '->B' + string.ascii_lowercase[-self.ndims:]
 
         #build fd stencil
         self.stencil = tf.constant(build_fd_coefficients(stencil_size, 2, ndims), dtype=tf.keras.backend.floatx())
@@ -101,6 +144,11 @@ class reverse_poisson_dataset_generator(tf.keras.utils.Sequence):
         
         #prepare padding variables if homogeneous bc is required
         self.homogeneous_bc = homogeneous_bc
+        if self.homogeneous_bc:
+            self._homogeneous_bc_control_pt_grid_paddings = tf.constant([[0,0],[0,0]] + [[1,1] for _ in range(ndims)], dtype=tf.int32)
+            soln_grid_padding_for_one_side = tf.constant([0,0] + list((tf.constant(self.stencil.shape[1:])//2).numpy()), dtype=tf.int32)
+            self._homogeneous_bc_soln_grid_paddings = tf.stack([soln_grid_padding_for_one_side, soln_grid_padding_for_one_side],1)
+
             
         #set max magnitude for outputs
         self.max_magnitude = max_magnitude
@@ -144,44 +192,89 @@ class reverse_poisson_dataset_generator(tf.keras.utils.Sequence):
         grid_sizes = (grid_size_range[:,1] - grid_size_range[:,0]) * grid_sizes + grid_size_range[:,0] + 1
         grid_sizes = tf.cast(grid_sizes, tf.int32)
         return grid_sizes
-
-    @tf.function
-    def adjust_grid_sizes_for_operator_application(self, grid_sizes):
-        return grid_sizes + self._convolution_input_size_reduction_amount
     
     @tf.function
-    def generate_soln(self):
+    def generate_soln_fourier(self):
         tiles = [self.batch_size] + [1 for _ in range(self.ndims)]
 
-        coeff_grid_size_range = tf.expand_dims(self.coeff_grid_size_range,0)
+        #determine how many Fourier modes should be in each sample in batch
+        coeff_grid_size_range = tf.expand_dims(self.fourier_coeff_grid_size_range,0)
         coeff_grid_size_range = tf.tile(coeff_grid_size_range,tiles)
         n_coefficients = tf.map_fn(self.generate_grid_sizes,coeff_grid_size_range,dtype=tf.int32)
 
+        #pick output grid size
         output_shape = self.generate_grid_sizes(self.output_size_range)
         output_shape = tf.expand_dims(output_shape,0)
         output_shape = tf.tile(output_shape,tiles[:2])
 
-        solns = tf.map_fn(lambda x: generate_smooth_function(x[0],x[1],self.homogeneous_bc),(output_shape,n_coefficients),dtype=tf.keras.backend.floatx())
+        #generate solutions (and get associated Fourier coefficients
+        solns, coeffs = tf.map_fn(lambda x: generate_smooth_function(x[0],x[1],homogeneous_bc = self.homogeneous_bc, normalize = False, return_coefficients = True, coefficients_return_shape = tf.reduce_max(n_coefficients,0)),(output_shape,n_coefficients),dtype=(tf.keras.backend.floatx(),tf.keras.backend.floatx()))
         
-        return tf.expand_dims(solns,1)
+        return tf.expand_dims(solns,1), coeffs
         
+    @tf.function
+    def generate_rhses_fourier(self, coefficients, grid_size):
+        #adjust coefficients from generate_soln_fourier to get RHS coefficients
+        grid_spacings = self.generate_grid_spacings()#pick a domain size for each sample
+        domain_sizes = tf.einsum('ij,j->ij',grid_spacings,tf.cast(grid_size,tf.keras.backend.floatx()))
+        if self.homogeneous_bc:#determine how many Fourier modes there are maximally
+            coefficients_size = tf.shape(coefficients)[1:]
+        else:
+            coefficients_size = tf.shape(coefficients)[2:]
+        wavenumbers = [tf.cast(tf.range(1,coefficients_size[k]+1),tf.keras.backend.floatx()) for k in range(self.ndims)]#generate (n+1)*pi values/wavenumbers for each coefficient
+        coefficient_adjustment = tf.stack(tf.meshgrid(*wavenumbers,indexing='ij'),0)*tf.constant(math.pi,tf.keras.backend.floatx())#adjust each coefficient by factor of [(n0+1)^2*pi^2/L0^2 + (n1+1)^2*pi^2/L1^2 + ...]
+        coefficient_adjustment = -tf.einsum('ij,j...->i...',(1/domain_sizes)**2,coefficient_adjustment**2)
+        if not self.homogeneous_bc:
+            coefficient_adjustment = tf.expand_dims(coefficient_adjustment,1)
+        coefficients = coefficients * coefficient_adjustment
+
+        grid_size = tf.expand_dims(grid_size,0)
+        grid_size = tf.tile(grid_size,[self.batch_size,1])
+
+        #get the RHS given the adjusted coefficients
+        rhs = tf.map_fn(lambda x: generate_smooth_function(x[0],x[1],homogeneous_bc = self.homogeneous_bc, normalize = False, return_coefficients = False), (grid_size, coefficients), dtype = tf.keras.backend.floatx())
+
+        rhs = tf.expand_dims(rhs,1)
+
+        return rhs, grid_spacings
 
     @tf.function
-    def generate_rhses_from_solutions(self, solutions):
-        grid_spacings = self.generate_grid_spacings()
-        if self.normalize_domain_size:
-            domain_sizes = tf.einsum('ij,j->ij',grid_spacings,tf.cast(tf.shape(solutions[self._soln_slice])[2:],grid_spacings.dtype))
-            max_domain_size = tf.reduce_max(domain_sizes,1)
-            grid_spacings = tf.einsum('ij,i->ij',grid_spacings,1/max_domain_size)
-        finite_difference_conv_kernels = tf.einsum('ij,j...->i...',1/(grid_spacings**2),self.stencil)#convert stencil to kernels
-        finite_difference_conv_kernels = tf.expand_dims(tf.expand_dims(finite_difference_conv_kernels, -1), -1)#adjust kernel dims for use with the conv method
-        rhses = tf.map_fn(lambda x: self.conv_method(x=x[0], kernel=x[1], data_format = 'channels_first')[0],(tf.expand_dims(solutions,1), finite_difference_conv_kernels), dtype=tf.keras.backend.floatx())#perform the conv for each
-        return rhses, grid_spacings
+    def build_taylor_rhs_component(self, vals, indices):
+        vals = [vals[k][indices[k]] for k in range(self.ndims)]
+        return tf.einsum(self.taylor_einsum_str,*vals)
+        
+    @tf.function
+    def generate_soln_and_rhs_taylor(self, grid_size, grid_spacings):
+        #generate polynomials and their 2nd derivatives along each direction
+        polynomials = []
+        second_derivatives = []
 
+        polynomial_degrees = self.generate_grid_sizes(self.taylor_degree_range)
+
+        domain_sizes = tf.einsum('ij,j->ij',grid_spacings,tf.cast(grid_size,tf.keras.backend.floatx()))
+        
+        for k in range(self.ndims):
+            coeffs = 2*tf.random.uniform([self.batch_size, polynomial_degrees[k]-1],dtype = tf.keras.backend.floatx())-1
+            p, ddp = polynomials_and_their_2nd_derivatives(grid_size[k], polynomial_degrees[k], domain_sizes[:,k], batch_size = self.batch_size, homogeneous_bc = self.homogeneous_bc)
+            p = tf.einsum('bij,bi->bj',p,coeffs)
+            ddp = tf.einsum('bij,bi->bj',ddp,coeffs)
+            polynomials.append(p)
+            ddp = tf.stack([p,ddp],0)
+            second_derivatives.append(ddp)
+        #build soln = X(x)Y(y)Z(z) etc
+        soln = tf.expand_dims(tf.einsum(self.taylor_einsum_str,*polynomials),1)
+
+        #build rhs = X"YZ + XY"Z + XYZ" etc
+        I = tf.eye(self.ndims,dtype = tf.int32)
+        rhs = tf.map_fn(lambda x: self.build_taylor_rhs_component(vals = second_derivatives, indices = x), I, dtype = tf.keras.backend.floatx())
+        rhs = tf.reduce_sum(rhs,0)
+        rhs = tf.expand_dims(rhs,1)
+        
+        return rhs, soln
+    
     @tf.function
     def pack_outputs(self, rhses, solns, grid_spacings):
-        solns = solns[self._soln_slice]
-
+        #create output tuples according to configuration
         problem_definition = []
         if self.return_rhses:
             problem_definition.append(rhses)
@@ -197,37 +290,47 @@ class reverse_poisson_dataset_generator(tf.keras.utils.Sequence):
 
     @tf.function
     def __getitem__(self, idx = 0):
-        solns = self.generate_soln()
-        rhses, grid_spacings = self.generate_rhses_from_solutions(solns)
+        #fourier component
+        solns_fourier, soln_coeffs_fourier = self.generate_soln_fourier()
+        rhses_fourier, grid_spacings_fourier = self.generate_rhses_fourier(soln_coeffs_fourier, tf.shape(solns_fourier)[2:])
+
+        #taylor series component
+        rhses_taylor, solns_taylor = self.generate_soln_and_rhs_taylor(tf.shape(solns_fourier)[2:], grid_spacings_fourier)
+        
+        rhses_taylor_max = tf.map_fn(lambda x: tf.reduce_max(tf.abs(x)),rhses_taylor)#scale taylor series component to that the max magnitude is identical to the fourier series component
+        rhses_fourier_max = tf.map_fn(lambda x: tf.reduce_max(tf.abs(x)),rhses_fourier)
+        taylor_scaling_coeffs = rhses_fourier_max/rhses_taylor_max
+        rhses_taylor = tf.einsum('i,i...->i...',taylor_scaling_coeffs, rhses_taylor)
+        solns_taylor = tf.einsum('i,i...->i...',taylor_scaling_coeffs, solns_taylor)
+
+        solns = solns_fourier + solns_taylor
+        rhses = rhses_fourier + rhses_taylor
+        grid_spacings = grid_spacings_fourier
+        
         out = self.pack_outputs(rhses, solns, grid_spacings)
         return out
 
 if __name__=='__main__':
     nmax = 1500
     nmin = 50
-    dmax = 1*1/(nmin-1)
-    dmin = 0.5*1/(nmax-1)
-    dxrange = [dmin,dmax]
+    dmax = (1e+0)*1/(nmin-1)
+    dmin = (1e+0)*1/(nmax-1)
+    dxrange = [(1e+0)*dmin,(1e+0)*dmax]
     ndims = 2
     grid_size_range = [[nmin,nmax] for _ in range(ndims)]
     cmax = 10
     cmin = 5
     ctrl_pt_range = [[cmin,cmax] for _ in range(ndims)]
-    rpdg = reverse_poisson_dataset_generator(10,10,grid_size_range,ctrl_pt_range,dxrange,homogeneous_bc = True, stencil_size = 5,normalize_domain_size = True)
-    for k in range(10):
-        inp, out = rpdg.__getitem__()
-        print('---')
-        print('max soln val ' + str(tf.reduce_max(tf.abs(out))))
-        print('max soln val divided by no of pts ' + str(tf.reduce_max(tf.abs(out))/tf.reduce_prod(tf.cast(tf.shape(inp[0])[2:],tf.float32))))
-        print('max rhs val ' + str(tf.reduce_max(tf.abs(inp[0]))))
-        print('max rhs val divided by no of pts ' + str(tf.reduce_max(tf.abs(inp[0]))/tf.reduce_prod(tf.cast(tf.shape(inp[0])[2:],tf.float32))))
-        sisd = tf.map_fn(lambda x: tf.reduce_max(tf.abs(x)),inp[0]) / tf.reduce_sum(1/(inp[-1]**2),1)
-        print('max rhs val divided by sum of inverse squares of dxes ' + str(sisd))
-        print('shape ' + str(tf.shape(inp[0])[2:]))
-    import pdb
-    pdb.set_trace()
-    '''
-    rpdg = reverse_poisson_dataset_generator(10,10,[[175,250],[100,150]],[[10,15],[10,15]],[1/249,1/99], homogeneous_bc = True, stencil_size = 5)
+    hbc = True
+    rpdg = reverse_poisson_dataset_generator(10,10,grid_size_range,ctrl_pt_range,ctrl_pt_range,grid_spacings_range=dxrange,homogeneous_bc = hbc, stencil_size = 5,normalize_domain_size = False)
     inp, out = rpdg.__getitem__()
-    '''
-    
+
+    rhs = inp[0]
+    soln = out
+    kernel = tf.einsum('i...,bi->b...',rpdg.stencil,1/(inp[-1]**2))
+    kernel = tf.reshape(kernel,tf.unstack(tf.shape(kernel)) + [1,1])
+    rhs_computed = tf.map_fn(lambda x:tf.keras.backend.conv2d(tf.expand_dims(x[0],0),x[1],data_format='channels_first'), (soln,kernel), dtype=soln.dtype)[:,0,...]
+    sl = [Ellipsis] + [slice(k//2,-(k//2)) for k in tf.shape(kernel)[1:3]]
+    rhs_analytical = rhs[sl]
+    print('MAPE between analytical (generated) RHS and finite difference RHS: ' + str(tf.reduce_mean((tf.abs(rhs_computed-rhs_analytical)/tf.abs(rhs_analytical))).numpy()))
+
