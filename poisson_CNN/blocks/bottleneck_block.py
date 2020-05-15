1b03b26f4705bae1a3f0772066b65c0c5da45372
Ali Girayhan Ozbay <aligirayhan.ozbay14@imperial.ac.uk>
2020-05-15 11:48:54 +0100

    documentation improvements

--- poisson_CNN/blocks/bottleneck_block.py
+++ poisson_CNN/blocks/bottleneck_block.py
@@ -10,7 +10,39 @@ def get_pooling_method(pool_downsampling_method, ndims):
     return eval(pooling_layer_name)
 
 class bottleneck_block(tf.keras.models.Model):
-    def __init__(self, ndims, downsampling_factor, previous_layer_filters, filters, conv_kernel_size, deconv_kernel_size, n_convs = 1, conv_padding_mode = 'constant', conv_constant_padding_value = 0.0, conv_conv_activation = tf.keras.activations.linear, conv_dense_activation = tf.keras.activations.linear, conv_pre_output_dense_units = [8,16], conv_use_bias = True, deconv_conv_activation = tf.keras.activations.linear, deconv_dense_activation = tf.keras.activations.linear, deconv_pre_output_dense_units = [8,16], deconv_use_bias = True, use_resnet = False, upsampling_factor = None, data_format = 'channels_first', conv_initializer_constraint_regularizer_options = {}, deconv_initializer_constraint_regularizer_options = {}, downsampling_method = 'conv', pool_downsampling_method = 'max'):
+    def __init__(self, ndims, downsampling_factor, previous_layer_filters, filters, conv_kernel_size, deconv_kernel_size, n_convs = 1, conv_padding_mode = 'constant', conv_constant_padding_value = 0.0, conv_conv_activation = tf.keras.activations.linear, conv_dense_activation = tf.keras.activations.linear, conv_pre_output_dense_units = [8,16], conv_use_bias = True, deconv_conv_activation = tf.keras.activations.linear, deconv_dense_activation = tf.keras.activations.linear, deconv_pre_output_dense_units = [8,16], deconv_use_bias = True, use_resnet = False, upsampling_factor = None, data_format = 'channels_first', conv_initializer_constraint_regularizer_options = {}, deconv_initializer_constraint_regularizer_options = {}, downsampling_method = 'conv', conv_downsampling_kernel_size = None, pool_downsampling_method = 'max', use_batchnorm = False, batchnorm_trainable = True):
+        '''
+        A block which downsamples the output by downsampling_factor, applies a convolution operation and upsamples.
+
+        Inputs:
+        -ndims: int. Number of spatial dimensions, can be 1 2 or 3.
+        -downsampling_factor: int or list of ints. Each dimension in the downsampled resolution will have dimension[k]//downsampling_factor[k] size.
+        -previous_layer_filters: int. The image supplied to this layer will have a # of channels identical to this argument.
+        -filters: int. The output will have this many channels.
+        -conv_kernel_size: int or list of ints. The shape of the kernel of the downsampled convolution operation(s) will be this.
+        -deconv_kernel_size: int or list of ints. The The shape of the kernel of the upsampling transposed convolution operation(s) will be this.
+        -n_convs: int. Number of convolution operations on the downsampled image.
+        -conv_padding_mode: string. see tf.pad mode argument.
+        -conv_constant_padding_value: float. determines tf.pad constant_values argument for conv layers.
+        -conv_conv_activation: callable. the activation function for the convolution operation(s) on the downsampled image.
+        -conv_dense_activation: callable or list of callables with identical size as conv_pre_output_dense_units. the activation functions for the feedforward NNs generating the conv kernels for the convs on the downsampled image.
+        -conv_pre_output_dense_units: list of ints. number of dense units in the intermediate layers of the feedforward NNs generating the conv kernels for the convs on the downsampled image.
+        -conv_use_bias: bool. determines if bias should be used in the conv operations on the downsampled image.
+        -deconv_conv_activation: callable. the activation function for the transposed convolution operation upsampling the downsampled image.
+        -deconv_dense_activation: callable or list of callables with identical size as conv_pre_output_dense_units. the activation functions for the feedforward NNs generating the conv kernels for the transposed convolution operation upsampling the downsampled image.
+        -deconv_pre_output_dense_units: callable or list of callables with identical size as deconv_pre_output_dense_units. the activation functions for the feedforward NN generating the conv kernels for the transposed convolution operation upsampling the downsampled image.
+        -deconv_use_bias: bool. determines if bias should be used in the transposed conv operation upsampling the downsampled image.
+        -use_resnet: bool. if enabled, all conv operations on the downsampled images will be resnet blocks.
+        -upsampling_factor: int or list of ints. determines the upsampling factor for the transposed conv operation. if set to None, the default value will be chosen as identical to the downsampling_factor argument so the original shape is recovered. when manually supplied, each spatial dim in the output will be input_dim[k]/downsampling_factor*upsampling_factor
+        -data_format: str. see keras docs.
+        -conv_initializer_constraint_regularizer_option: dict. provide kernel_initializer, kernel_constraint etc arguments in this dict for the conv layers.
+        -deconv_initializer_constraint_regularizer_option: dict. provide kernel_initializer, kernel_constraint etc arguments in this dict for the deconv layer
+        -downsampling_method: str, 'conv' or 'pool'. use a strided convolution to do downsampling if 'conv' is chosen and pooling if 'pool' is chosen.
+        -conv_downsampling_kernel_size: int. kernel size for the conv downsampling method. if not supplied and 'conv' downsampling is chosen, it will be set to conv_kernel_size.
+        -pool_downsampling_method: str, 'max' or 'average'. if pool downsampling is chosen, determines if max or average pooling should be used.
+        -use_batchnorm: bool. If set to true, a batchnorm layer is added after the transposed conv.
+        -batchnorm_trainable: bool. If set to true, the batchnorm layer will be trainable (see keras docs).
+        '''
 
         super().__init__()
         
@@ -26,9 +58,12 @@ class bottleneck_block(tf.keras.models.Model):
         self.conv_layers = []
             
         conv_input_args = {'dimensions': ndims, 'previous_layer_filters': filters, 'filters': filters, 'kernel_size': conv_kernel_size, 'strides': None, 'dilation_rate': None, 'padding': 'same', 'padding_mode': conv_padding_mode, 'constant_padding_value': conv_constant_padding_value, 'data_format': data_format, 'conv_activation': conv_conv_activation, 'dense_activations': conv_dense_activation, 'pre_output_dense_units': conv_pre_output_dense_units, 'use_bias': conv_use_bias}
-        
+
+        self.downsampling_method = downsampling_method
         if downsampling_method == 'conv':
-            downsampling_input_args = {'dimensions':ndims,'previous_layer_filters': previous_layer_filters, 'filters': filters, 'kernel_size': conv_kernel_size, 'strides': downsampling_factor, 'dilation_rate': None, 'padding': 'same', 'padding_mode': conv_padding_mode, 'constant_padding_value': conv_constant_padding_value, 'data_format': data_format, 'conv_activation': conv_conv_activation, 'dense_activations': conv_dense_activation, 'pre_output_dense_units': conv_pre_output_dense_units, 'use_bias': conv_use_bias}
+            if conv_downsampling_kernel_size is None:
+                conv_downsampling_kernel_size = conv_kernel_size
+            downsampling_input_args = {'dimensions':ndims,'previous_layer_filters': previous_layer_filters, 'filters': filters, 'kernel_size': conv_downsampling_kernel_size, 'strides': downsampling_factor, 'dilation_rate': None, 'padding': 'same', 'padding_mode': conv_padding_mode, 'constant_padding_value': conv_constant_padding_value, 'data_format': data_format, 'conv_activation': conv_conv_activation, 'dense_activations': conv_dense_activation, 'pre_output_dense_units': conv_pre_output_dense_units, 'use_bias': conv_use_bias}
             downsampling_input_args = {**downsampling_input_args, **conv_initializer_constraint_regularizer_options}#merges dicts
             self.downsample_layer = metalearning_conv(**downsampling_input_args)
         elif downsampling_method == 'pool':
@@ -52,12 +87,19 @@ class bottleneck_block(tf.keras.models.Model):
         upsampling_input_args = {**upsampling_input_args, **deconv_initializer_constraint_regularizer_options}
 
         self.upsample_layer = metalearning_deconvupscale(**upsampling_input_args)
+        self.use_batchnorm = use_batchnorm
+        if use_batchnorm:
+            self.batchnorm = tf.keras.layers.BatchNormalization(axis = 1 if self.data_format == 'channels_first' else -1, trainable = batchnorm_trainable)
 
+            
     @tf.function
     def call(self,inp):
         conv_inp, dense_inp = inp
 
-        out = self.downsample_layer([conv_inp,dense_inp])
+        if self.downsampling_method == 'conv':
+            out = self.downsample_layer([conv_inp,dense_inp])
+        else:
+            out = self.downsample_layer(conv_inp)
 
         for layer in self.conv_layers:
             out = layer([out,dense_inp])
@@ -72,6 +114,9 @@ class bottleneck_block(tf.keras.models.Model):
 
         out = self.upsample_layer([out,dense_inp,outshape])
 
+        if self.use_batchnorm:
+            out = self.batchnorm(out)
+
         return out
 
 if __name__ == '__main__':
@@ -79,13 +124,13 @@ if __name__ == '__main__':
     n_prevch = 2
     n_postch = 4
     bsize = 10
-    conv_inp = tf.random.uniform([bsize,n_prevch] + [250 for _ in range(ndims)])
+    conv_inp = tf.random.uniform([bsize,n_prevch] + [2500 for _ in range(ndims)])
     dense_inp = tf.random.uniform((10,10))
 
-    downsampling_factor = 2
-    upsampling_factor = 3
-    conv_ksize = 5
-    deconv_ksize = 5
+    downsampling_factor = [6,4]
+    upsampling_factor = [3,3]
+    conv_ksize = 6
+    deconv_ksize = 6
     n_convs = 3
     conv_padding_mode = 'SYMMETRIC'
     conv_activations = tf.nn.leaky_relu
